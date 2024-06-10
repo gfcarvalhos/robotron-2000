6fd3f6bfd499de54dbc6d16e8c6ada2753e9a7e0
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-06-09 22:17:30 -0300

    :construction: feat: add data change for all body part from robot

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
-const robotron = document.querySelector('#robotron');
-const subtrair = document.querySelector('#subtrair');
-const somar = document.querySelector('#somar');
-const braco = document.querySelector('#braco');
-
 const controle = document.querySelectorAll('.controle-ajuste');
 
 controle.forEach((elemento) => {
   elemento.addEventListener('click', (evento) => {
-    manipulaDados(evento.target.textContent);
-    console.log(elemento);
+    manipulaDados(evento.target.textContent, evento.target.parentNode);
   });
 });
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector('.controle-contador');
+
   if (operacao === '-') {
-    braco.value = parseInt(braco.value) - 1;
+    peca.value = parseInt(peca.value) - 1;
   } else {
-    braco.value = parseInt(braco.value) + 1;
+    peca.value = parseInt(peca.value) + 1;
   }
 }
-
-// function dizOi() {
-//   console.log('oi');
-//   console.log('Bem-vindo ao Robotron!');
-// }
-
-// dizOi();
