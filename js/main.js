1d91eba55475a4a4e53f4c85f200eab6fa7192df
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-06-09 22:06:00 -0300

    feat: add counter to arms build

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener('click', dizOi);
+const controle = document.querySelectorAll('.controle-ajuste');
 
-function dizOi() {
-  console.log('oi');
+controle.forEach((elemento) => {
+  elemento.addEventListener('click', (evento) => {
+    manipulaDados(evento.target.textContent);
+    console.log(elemento);
+  });
+});
+
+function manipulaDados(operacao) {
+  if (operacao === '-') {
+    braco.value = parseInt(braco.value) - 1;
+  } else {
+    braco.value = parseInt(braco.value) + 1;
+  }
 }
+
+// function dizOi() {
+//   console.log('oi');
+//   console.log('Bem-vindo ao Robotron!');
+// }
+
+// dizOi();
